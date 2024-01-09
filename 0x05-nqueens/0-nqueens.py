84b39568e9d1ec1b43746c09931cd75addef2c69
Engrkenzie <113719170+Engrkenzie@users.noreply.github.com>
2024-01-09 11:37:07 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,61 +1,43 @@
 #!/usr/bin/python3
-"""Solution to the N-Queens puzzle"""
 import sys
 
 
-def print_board(board, n):
-    """prints allocated possitions to the queen"""
-    b = []
-
-    for i in range(n):
-        for j in range(n):
-            if j == board[i]:
-                b.append([i, j])
-    print(b)
-
-
-def safe_position(board, i, j, r):
-    """Determines whether the position is safe for the queen"""
-    if (board[i] == j) or (board[i] == j - i + r) or (board[i] == i - r + j):
-        return True
-    return False
-
-
-def determine_positions(board, row, n):
-    """Recursively finds all safe positions where the queen can be allocated"""
-    if row == n:
-        print_board(board, n)
-
-    else:
-        for j in range(n):
-            allowed = True
-            for i in range(row):
-                if safe_position(board, i, j, row):
-                    allowed = False
-            if allowed:
-                board[row] = j
-                determine_positions(board, row + 1, n)
+def nqueens(n):
+    """
+    The N queens puzzle is the challenge
+    of placing N non-attacking queens on an N×N chessboard
+    """
+    def nqueensv2(queens, cord_dif, cord_sum):
+        """ Recursive function
+        """
+        p = len(queens)
+        if p == n:
+            result.append(queens)
+            return None
+        for q in range(n):
+            if q not in queens and p-q not in cord_dif and p+q not in cord_sum:
+                nqueensv2(queens + [q], cord_dif + [p - q], cord_sum + [p + q])
+    result = []
+    final_result = []
+    nqueensv2([], [], [])
+    for row in result:
+        for i, col in enumerate(row):
+            coord = [i, col]
+            final_result.append(coord)
+        print(final_result)
+        final_result = []
 
 
-def create_board(size):
-    """Generates the board"""
-    return [0 * size for i in range(size)]
-
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-try:
-    n = int(sys.argv[1])
-except BaseException:
-    print("N must be a number")
-    exit(1)
-
-if (n < 4):
-    print("N must be at least 4")
-    exit(1)
-
-board = create_board(int(n))
-row = 0
-determine_positions(board, row, int(n))
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+    nqueens(n)
